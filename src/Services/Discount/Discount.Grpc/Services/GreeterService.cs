3a454961d244d911b76ecb9e2fc7db24f0ecfbc8
dimash.abdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-06-21 22:41:17 +0500

    discount init

---
+++ src/Services/Discount/Discount.Grpc/Services/GreeterService.cs
@@ -0,0 +1,17 @@
+using Grpc.Core;
+using Discount.Grpc;
+
+namespace Discount.Grpc.Services;
+
+public class GreeterService(ILogger<GreeterService> logger) : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger = logger;
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
