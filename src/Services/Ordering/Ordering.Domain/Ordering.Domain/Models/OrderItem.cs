72c2645fc8f3b7441d5793e2e76953f8782534df
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-07-02 23:02:35 +0500

    ordering models, value objects, start customer + customerId rich models

---
+++ src/Services/Ordering/Ordering.Domain/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<OrderItemId>
+{
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+    {
+        // Id = OrderItemId.Of(Guid.NewGuid());
+        OrderId = orderId.Value;
+        ProductId = productId.Value;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public Guid OrderId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+}
---
+++ src/Services/Ordering/Ordering.Domain/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string? CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
+    public string Cvv { get; set; } = default!;
+    public int PaymentMethod { get; set; } = default!;
+}
