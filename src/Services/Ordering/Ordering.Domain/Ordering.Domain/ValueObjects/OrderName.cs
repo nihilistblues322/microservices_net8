60916e5d39a57585577f46172595c0c3fd613e86
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-07-03 11:27:32 +0500

    ordering rish val obj

--- src/Services/Ordering/Ordering.Domain/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/Ordering.Domain/ValueObjects/OrderName.cs
@@ -2,5 +2,17 @@
 
 public record OrderName
 {
+    private const int DefaultLength = 5;
+
     public string Value { get; }
+
+    private OrderName(string value) => Value = value;
+
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength, "OrderName must be 5 characters long");
+
+        return new OrderName(value);
+    }
 }
--- src/Services/Ordering/Ordering.Domain/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/Ordering.Domain/ValueObjects/Payment.cs
@@ -7,4 +7,27 @@ public record Payment
     public string Expiration { get; set; } = default!;
     public string Cvv { get; set; } = default!;
     public int PaymentMethod { get; set; } = default!;
+
+    protected Payment()
+    {
+    }
+
+    private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+    {
+        CardName = cardName;
+        CardNumber = cardNumber;
+        Expiration = expiration;
+        Cvv = cvv;
+        PaymentMethod = paymentMethod;
+    }
+
+    public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3, "Cvv must be 3 characters long");
+
+        return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+    }
 }
