6780a842f5e479113b2f06221e783a29751538c5
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-07-11 22:58:14 +0500

    ordering api endpoints

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.API.Endpoints;
+
+public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByName : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+                var response = result.Adapt<GetOrdersByNameResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetOrdersByName")
+            .Produces<GetOrdersByNameResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Orders By Name")
+            .WithDescription("Get Orders By Name");
+    }
+}
