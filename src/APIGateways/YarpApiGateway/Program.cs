c1f36b22f0fc4aa2d969aa1296044e24a5a4a793
dimashabdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-08-16 15:21:24 +0500

    added yarp route clusters

--- src/APIGateways/YarpApiGateway/Program.cs
+++ src/APIGateways/YarpApiGateway/Program.cs
@@ -1,5 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
+app.MapReverseProxy();
 
 app.Run();
